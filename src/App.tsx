a3a1bea6c824caf760f832d5981d6833b73013af
dasha <dasha@skv.name>
2024-03-07 11:53:09 +0300

    added vkui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import './App.css';
+import { createRoot } from 'react-dom/client';
+import {
+  AdaptivityProvider,
+  ConfigProvider,
+  AppRoot,
+  SplitLayout,
+  SplitCol,
+  View,
+  Panel,
+  PanelHeader,
+  Header,
+  Group,
+  SimpleCell,
+  usePlatform,
+} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
 
-function App() {
+export default function App() {
+  const platform = usePlatform();
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <AppRoot>
+      <SplitLayout header={platform !== 'vkcom' && <PanelHeader delimiter="none" />}>
+        <SplitCol autoSpaced>
+          <View activePanel="main">
+            <Panel id="main">
+              <PanelHeader>VK Groups</PanelHeader>
+              <Group header={<Header mode="secondary">Groups</Header>}>
+                <SimpleCell>Hello</SimpleCell>
+                <SimpleCell>World</SimpleCell>
+              </Group>
+            </Panel>
+          </View>
+        </SplitCol>
+      </SplitLayout>
+    </AppRoot>
   );
-}
-
-export default App;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AdaptivityProvider, ConfigProvider } from '@vkontakte/vkui';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <ConfigProvider>
+        <AdaptivityProvider>
+          <App />
+        </AdaptivityProvider>
+      </ConfigProvider>,
   </React.StrictMode>
 );
